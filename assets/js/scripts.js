57854fd4777e2529d5de9d72d436d315b2dbd15c
themaryanjuguna <themaryanjuguna@gmail.com>
2022-03-14 04:32:07 +0300

    form layout

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,3 +4,13 @@ $(document).ready(function(){
     })
 })
 
+
+$("delaniForm" ).submit(function(event) {
+    if ( $("input").first().val() === "correct" ) {
+      $( "span" ).text( "Thank you. Your message has been recieved" ).show();
+      return;
+    }
+   
+    $("span").text( "Not valid!" ).show().fadeOut( 1000 );
+    event.preventDefault();
+  });
